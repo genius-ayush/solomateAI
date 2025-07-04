e5d29b0903b0b49336902a7136bfa67214fcc447
genius-ayush <ayushsanjayrawal@gmail.com>
2025-07-04 22:56:57 +0530

    worked on landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,17 @@
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
       <Header/>
       <Hero/>
+      <Features/>
+      <Footer/>
     </div>
   );
 }
+// 1b172b
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,3 +1,4 @@
+import { discordBlack, facebook, instagram, roadmap1, roadmap2, roadmap3, roadmap4, telegram, twitter } from "@/public/assets";
 
 
 export const navigation = [
@@ -28,3 +29,72 @@ export const navigation = [
         onlyMobile: true
     }
 ]
+
+export const socials = [
+    {
+      id: "0",
+      title: "Discord",
+      iconUrl: discordBlack,
+      url: "#",
+    },
+    {
+      id: "1",
+      title: "Twitter",
+      iconUrl: twitter,
+      url: "#",
+    },
+    {
+      id: "2",
+      title: "Instagram",
+      iconUrl: instagram,
+      url: "#",
+    },
+    {
+      id: "3",
+      title: "Telegram",
+      iconUrl: telegram,
+      url: "#",
+    },
+    {
+      id: "4",
+      title: "Facebook",
+      iconUrl: facebook,
+      url: "#",
+    },
+  ];
+
+  export const roadmap = [
+    {
+      id: "0",
+      title: "Empirical Iteration",
+      text: "Our research embraces rapid hypothesis testing and continuous refinement, prioritizing methodical iteration on findings over single interventions",
+      date: "May 2023",
+      status: "done",
+      imageUrl: roadmap1,
+      colorful: true,
+    },
+    {
+      id: "1",
+      title: "Human Expertise Primacy",
+      text: "AWe hold that human-generated data contains cognitive patterns and expertise that cannot be replicated through synthetic generation or web scraping.",
+      date: "May 2023",
+      status: "progress",
+      imageUrl: roadmap2,
+    },
+    {
+      id: "2",
+      title: "Practitioner Verification",
+      text: "We maintain rigorous standards for domain experts, ensuring validation by individuals with demonstrated field expertise.",
+      date: "May 2023",
+      status: "done",
+      imageUrl: roadmap3,
+    },
+    {
+      id: "3",
+      title: "Adaptive Methodology",
+      text: "Our approach scales dynamically to address both targeted capability gaps and broader questions about AI functionality.",
+      date: "May 2023",
+      status: "progress",
+      imageUrl: roadmap4,
+    },
+  ];
